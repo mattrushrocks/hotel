656698a78246e4e2b42348a07748bfdd1b45e19f
Matt Rushforth <matthewrushforth12@gmail.com>
2025-12-05 23:29:44 -0700

    first initial

---
+++ js/hoteldata.js
@@ -0,0 +1,74 @@
+export const hotels = [
+  {
+    "image": "images/hotel1.webp",
+    "name": "Hogwarts Highlands Inn",
+    "phone": "+44 0207 555 1001",
+    "address": [
+      "North Approach to Hogwarts Grounds",
+      "Near Hogsmeade Village, Scotland"
+    ]
+  },
+  {
+    "image": "images/hotel2.webp",
+    "name": "Three Broomsticks Lodge",
+    "phone": "+44 0207 555 1002",
+    "address": [
+      "Adjacent to The Three Broomsticks",
+      "High Street, Hogsmeade"
+    ]
+  },
+  {
+    "image": "images/hotel3.webp",
+    "name": "The Cauldron & Quill Inn",
+    "phone": "+44 0207 555 1003",
+    "address": [
+      "42 Spellmaker’s Row",
+      "Diagon Alley, London"
+    ]
+  },
+  {
+    "image": "images/hotel4.webp",
+    "name": "Leaky Cauldron Wizarding Suites",
+    "phone": "+44 0207 555 1004",
+    "address": [
+      "1 Charing Cross Approach",
+      "Behind The Leaky Cauldron, London"
+    ]
+  },
+  {
+    "image": "images/hotel5.webp",
+    "name": "Hog's Head Hearth & Stay",
+    "phone": "+44 0207 555 1005",
+    "address": [
+      "Back Lane off High Street",
+      "Hogsmeade Village"
+    ]
+  },
+  {
+    "image": "images/hotel6.webp",
+    "name": "Black Lake Lumos Resort",
+    "phone": "+44 0207 555 1006",
+    "address": [
+      "Shoreline Path, Black Lake",
+      "Hogwarts Grounds, Scotland"
+    ]
+  },
+  {
+    "image": "images/hotel7.webp",
+    "name": "Forbidden Forest Retreat",
+    "phone": "+44 0207 555 1007",
+    "address": [
+      "East Edge of the Forbidden Forest",
+      "Near The Gamekeeper’s Hut, Scotland"
+    ]
+  },
+  {
+    "image": "images/hotel8.webp",
+    "name": "Platform Nine & Three-Quarters Hostel",
+    "phone": "+44 0207 555 1008",
+    "address": [
+      "Kings Cross Station Annex",
+      "London, England"
+    ]
+  }
+];
---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+const hambtn = document.querySelector("#hamburgerBtn")
+const pagewrap = document.querySelector("#pageWrapper")
+
+hambtn.addEventListener('click', ()=> {
+    pagewrap.classList.toggle('moveOver')
+
+});
+
+import { hotels } from "./hoteldata.js";
+//console.log(hotels)
+const hotelCard =document.querySelector('#cards')
+hotels.forEach (hotel => {
+    const mySection = document.createElement("section")
+
+    const myImg = document.createElement("img")
+    myImg.src = `${hotel.image}`
+    myImg.alt = hotel.name
+
+    const theName = document.createElement("h2")
+    theName.textContent = hotel.name
+
+    const theAddress = document.createElement('address');
+    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+    const thePhone = document.createElement('a')
+    thePhone.textContent = hotel.phone
+    thePhone.href = `tel:${hotel.phone}`
+
+    mySection.appendChild(myImg)
+    mySection.appendChild(theName)
+    mySection.appendChild(theAddress)
+    mySection.appendChild(thePhone)
+    hotelCard.appendChild(mySection)
+
+})
