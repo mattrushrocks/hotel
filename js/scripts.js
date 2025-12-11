2d2ed714881c4739a0fad0799a8a7ae433f1615f
Matt Rushforth <matthewrushforth12@gmail.com>
2025-12-10 19:20:55 -0700

    Fixed the broken page lol

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,40 @@
-const hambtn = document.querySelector("#hamburgerBtn")
-const pagewrap = document.querySelector("#pageWrapper")
+// Hamburger button toggle
+const hambtn = document.querySelector("#hamburgerBtn");
+const pagewrap = document.querySelector("#pageWrapper");
 
-hambtn.addEventListener('click', ()=> {
-    pagewrap.classList.toggle('moveOver')
+if (hambtn && pagewrap) {
+  hambtn.addEventListener("click", () => {
+    pagewrap.classList.toggle("moveOver");
+    console.log("Hamburger toggled:", pagewrap.classList.contains("moveOver"));
+  });
+}
 
-});
-
+// Hotel cards
 import { hotels } from "./hoteldata.js";
-//console.log(hotels)
-const hotelCard =document.querySelector('#cards')
-hotels.forEach (hotel => {
-    const mySection = document.createElement("section")
 
-    const myImg = document.createElement("img")
-    myImg.src = `${hotel.image}`
-    myImg.alt = hotel.name
+const hotelCard = document.querySelector("#cards");
 
-    const theName = document.createElement("h2")
-    theName.textContent = hotel.name
+hotels.forEach(hotel => {
+  const mySection = document.createElement("section");
 
-    const theAddress = document.createElement('address');
-    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+  const myImg = document.createElement("img");
+  myImg.src = hotel.image;
+  myImg.alt = hotel.name;
 
-    const thePhone = document.createElement('a')
-    thePhone.textContent = hotel.phone
-    thePhone.href = `tel:${hotel.phone}`
+  const theName = document.createElement("h2");
+  theName.textContent = hotel.name;
 
-    mySection.appendChild(myImg)
-    mySection.appendChild(theName)
-    mySection.appendChild(theAddress)
-    mySection.appendChild(thePhone)
-    hotelCard.appendChild(mySection)
+  const theAddress = document.createElement("address");
+  theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
 
-})
+  const thePhone = document.createElement("a");
+  thePhone.textContent = hotel.phone;
+  thePhone.href = `tel:${hotel.phone}`;
+
+  mySection.appendChild(myImg);
+  mySection.appendChild(theName);
+  mySection.appendChild(theAddress);
+  mySection.appendChild(thePhone);
+
+  hotelCard.appendChild(mySection);
+});
